285b40c15ea6a92b585ba631cafc2adf85522073
jzhang <jzhang@xogrp.com>
2018-04-17 23:34:42 +0800

    add none js function

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
+  end
+
+  def none_js
+    `echo #{params[:msg]} >> chat.log`
+    @chat_log = `cat chat.log`
   end
 end
